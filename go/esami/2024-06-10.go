247206516877979992a813fafba33525ba790303
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2024-12-25 17:51:10 -0500

    refactor: autolavaggio, manca la richiesta personale

--- go/esami/2024-06-10.go
+++ go/esami/2024-06-10.go
@@ -1,8 +1,5 @@
 package main
 
-//ben impostato ma qui c'è un grande problema ovvero le risorse condivise sono autonome e devi sempre usare indici ovunque
-//esempio: come faccio a sapere se non posso entrare nel tunnel da cliente... dipende quale
-
 import (
 	"fmt"
 	"math/rand"
@@ -10,225 +7,225 @@ import (
 	"strings"
 )
 
-type req struct {
-	id int
+type richiesta struct {
+	soggetto int
+	oggetto int
 	ack chan int
 }
 
-type tunnel struct {
-	addetto int
-	cliente int
-}
-
 const (
-	NUM_CLIENTI = 100
-	NUM_ADDETTI = 10
-	NUM_TUNNELS  = 10
-	NUM_PI      = 30
-	
+	NON_SIGNIFICATIVO = -1
+
 	MAX      = 30
 	MAX_BUFF = 100
 	
-	TEMPO_MIN     = 500
-	TEMPO_INIZIO  = 500
-	TEMPO_CLIENTE  = 1000
+	NUM_PI      = 30
+	NUM_TUNNELS = 10
+
+	NUM_CLIENTI = 100
+	NUM_ADDETTI = 10
+	
+	TEMPO_MINIMO  = 500
 	TEMPO_ADDETTO = 3000
+	TEMPO_CLIENTE = 1000
+	
+	AZIONI_ADDETTO = 2
+	AZIONI_CLIENTE = 2
+	
+	TIPI_CLIENTE = 2
 )
 
 var (
-	entraAddetto        = make(chan req, MAX_BUFF)
-	esceAddetto         = make(chan req, MAX_BUFF)
-	filtratoEsceAddetto = make(chan req, MAX_BUFF)
-	terminaFiltro       = make(chan chan bool)
-
-	entraCliente = [2]chan req{make(chan req, MAX_BUFF), make(chan req, MAX_BUFF)}
-	esceCliente  = [2]chan req{make(chan req, MAX_BUFF), make(chan req, MAX_BUFF)}
+	canaliAddetto = [AZIONI_ADDETTO]chan richiesta{
+					 make(chan richiesta, MAX_BUFF),
+					 make(chan richiesta, MAX_BUFF)}
+					 
+	canaliCliente = [TIPI_CLIENTE][AZIONI_CLIENTE]chan richiesta{
+					{make(chan richiesta, MAX_BUFF), make(chan richiesta, MAX_BUFF)},
+					{make(chan richiesta, MAX_BUFF), make(chan richiesta, MAX_BUFF)}}
 
 	finito              = make(chan bool, MAX_BUFF)
 	bloccaAutolavaggio  = make(chan bool)
 	terminaAutolavaggio = make(chan bool)
 )
 
-func when(b bool, c chan req) chan req {
-	if !b {
-		return nil
+func lunghezzeCanaliInVettore(canali []chan richiesta) []int {
+	lunghezze := make([]int, len(canali))
+	for i, c := range canali { lunghezze[i] = len(c) }
+	return lunghezze
+}
+
+func lunghezzeCanaliInMatrice(canali [][]chan richiesta) [][]int {
+	lunghezze := make([][]int, len(canali))
+	for i, riga := range canali {
+		lunghezze[i] = make([]int, len(riga))
+		for j, c := range riga { lunghezze[i][j] = len(c) }
 	}
+	return lunghezze
+}
+
+func when(b bool, c chan req) chan req {
+	if !b { return nil }
 	return c
 }
 
+func priorità(canali ...chan req) bool {
+	for _, c := range canali { if len(c) > 0 { return false } }
+	return true
+}
+
 func autolavaggio() {
-	fmt.Println("[AUTOLAVAGGIO] inizio")
+	const nome = "AUTOLAVAGGIO"
+	var spazi = strings.Repeat(" ", len(nome) + 3)
+	fmt.Printf("[%s] inizio\n", nome)
 
 	var (
-		addetti = 0
 		clientiTunnel = 0
 		clientiPI = 0
 		
 		fine = false
 	)
 	
-	var tunnels = [NUM_TUNNELS]tunnel{}
-	for i := 0; i < NUM_TUNNELS; i++ {
-		tunnels[i] = tunnel{addetto: -1, cliente: -1}
-	}
-	
-	trova := func(id int) int {
-		for i := 0; i < NUM_TUNNELS; i++ {
-			if (tunnels[i].addetto != -1 && tunnels[i].cliente == id) {
-				return i
-			}
-		}
+    var tunnels [NUM_TUNNELS]int
+    for i := range tunnels { tunnels[i] = -2 }
+    
+	disponibile := func() int {
+		for i := 0; i < NUM_TUNNELS; i++ { if (tunnels[i] == -1) { return i } }
 		return -1
 	}
-	libero := func() bool {
- 		return clientiTunnel + clientiPI < MAX
-	}
 	
-	filtraEsceAddetto := func() {
-		for {
-		    select {
-		    case r := <-esceAddetto: {
-		        if tunnels[r.id].cliente == -1 {
-		            filtratoEsceAddetto <- r
-		        }
-		    }
-		    case ack:= <-terminaFiltro: {
-		        ack <- true
-		        return
-		    }}
-		}
-	}
-	go filtraEsceAddetto()
+	libero := func() bool { return clientiTunnel + clientiPI < MAX }
+	
+	liberaPI := func() bool { return clientiPI < NUM_PI }
 
 	for {
-		fmt.Printf("[AUTOLAVAGGIO] Addetti: %1d, Clienti Tunnel: %03d, Clienti PI: %03d, EntraA: %1d, EsceA: %1d, EntraCT: %03d, EsceCT: %03d, EntraCPI: %03d, EsceCPI: %03d\n", addetti, clientiTunnel, clientiPI, len(entraAddetto), len(esceAddetto), len(entraCliente[0]), len(esceCliente[0]), len(entraCliente[1]), len(esceCliente[1]))
+		var canaliClienteSlice = make([][]chan richiesta, len(canaliCliente))
+		for i, row := range canaliCliente { canaliClienteSlice[i] = append([]chan richiesta(nil), row[:]...) }
+		var (
+			lunghezzeAddetto = lunghezzeCanaliInVettore(canaliAddetto[:])
+			lunghezzeCliente = lunghezzeCanaliInMatrice(canaliClienteSlice)
+		)
+		fmt.Printf("[%s] clientiTunnel: %03d, clientiPI: %03d, fine: %5t\n%sCanaliAddetto: %v, CanaliCliente: %v\n",
+		nome, clientiTunnel, clientiPI, fine, spazi, lunghezzeAddetto, lunghezzeCliente)
+		
 		select {
 		case r := <-when(!fine,
-		entraAddetto): {
-			tunnels[r.id].addetto = r.id
-			addetti++
-			r.ack <- r.id
+		canaliAddetto[0]): {
+			tunnels[r.soggetto] = -1
+			r.ack <- 1
 		}
 		case r := <-when(fine,
-		entraAddetto): {
+		canaliAddetto[0]): {
 			r.ack <- -1
 		}
-		case r := <-filtratoEsceAddetto: {
-			tunnels[r.id].addetto = -1
-			addetti--
-			r.ack <- r.id
+		case r := <-when(//non so come fare qui devo esprimere una condizione su r
+		canaliAddetto[1]): {
+			tunnels[r.soggetto] = -2
+			r.ack <- 1
 		}
-		case r := <-when(len(esceAddetto) == 0 && len(entraCliente[1]) == 0 && libero(),
-		entraCliente[0]): {
-			var id = trova(-1)
-			tunnels[id].cliente = r.id
+		case r := <-when(libero() && disponibile() >= 0 &&
+		priorità(canaliAddetto[1], canaliCliente[1][0]),
+		canaliCliente[0][0]): {
+			var oggetto = disponibile()
+			tunnels[oggetto] = r.soggetto
 			clientiTunnel++
-			r.ack <- id
+			r.ack <- oggetto
 		}
-		case r := <-esceCliente[0]: {
-			var id = trova(r.id)
-			tunnels[id].cliente = -1
+		case r := <-canaliCliente[0][1]: {
+			tunnels[r.oggetto] = -1
 			clientiTunnel--
-			r.ack <- id
+			r.ack <- 1
 		}
-		case r := <-when(libero(),
-		entraCliente[1]): {
+		case r := <-when(libero() && liberaPI(),
+		canaliCliente[1][0]): {
 			clientiPI++
-			r.ack <- r.id
+			r.ack <- 1
 		}
-		case r := <-esceCliente[1]: {
+		case r := <-canaliCliente[1][1]: {
 			clientiPI--
-			r.ack <- r.id
+			r.ack <- 1
 		}
 		case <-bloccaAutolavaggio: {
 			fine = true
 		}
 		case <-terminaAutolavaggio: {
-			var ack = make(chan bool)
-			terminaFiltro <- ack
-			<-ack
 			finito <- true
-			fmt.Println("[AUTOLAVAGGIO] fine")
+			fmt.Printf("[%s] fine\n", nome)
 			return
 		}}
 	}
 }
 
 func addetto(id int) {
-	fmt.Printf("[ADDETTO %03d] inizio\n", id)
+	const nome = "ADDETTO"
+	fmt.Printf("[%s %03d] inizio\n", nome, id)
 
-	// preparazione
-	r := req{id: id, ack: make(chan int)}
-	
-	// comportamento
-	var continua int 
+	var (
+		r = richiesta { soggetto: id, oggetto: NON_SIGNIFICATIVO, ack: make(chan int) }
+		azioni = [AZIONI_ADDETTO]string {"entrare in servizio", " riposare"}
+		continua int
+	)
 	for {
-		time.Sleep(time.Duration(rand.Intn(TEMPO_INIZIO)+TEMPO_MIN) * time.Millisecond)
-		fmt.Println("[ADDETTO %03d] mi metto in coda per usare il tunnel", id) 
-		entraAddetto <- r
-		continua = <-r.ack
-		if continua > 0 {
-			finito <- true
-			fmt.Printf("[ADDETTO %03d] fine\n", id)
-			return
+		for i, azione := range azioni {
+			time.Sleep(time.Duration(rand.Intn(TEMPO_ADDETTO) + TEMPO_MINIMO) * time.Millisecond)
+			fmt.Printf("[%s %03d] mi metto in coda per %s\n", nome, id, azione)
+			canaliAddetto[i] <- ack
+			continua = <-ack
+			if continua < 0 {
+				finito <- true
+				fmt.Printf("[%s %03d] fine\n", nome, id)
+				return
+			}
+			fmt.Printf("[%s %03d] è il mio turno di %s\n", nome, id, azione)
 		}
-		fmt.Println("[ADDETTO %03d] è il mio turno di usare il tunnel", id)
-		time.Sleep(time.Duration(rand.Intn(TEMPO_ADDETTO)+TEMPO_MIN) * time.Millisecond)
-		fmt.Println("[ADDETTO %03d] mi metto in coda per uscire dal tunnel", id) 
-		esceAddetto <- r
-		<-r.ack
-		fmt.Println("[ADDETTO %03d] è il mio turno di uscire dal tunnel", id)
 	}
 }
 
 func cliente(id int, tipo int) {
-	time.Sleep(time.Duration(rand.Intn(TEMPO_INIZIO)+TEMPO_MIN) * time.Millisecond)
-	fmt.Printf("[CLIENTE %03d] inizio\n", id)
+	const nome = "CLIENTE"
+	fmt.Printf("[%s %03d] inizio\n", nome, id)
 	
-	// preparazione
-	r := req{id: id, ack: make(chan int)}
-	
-	// comportamento
-	var desideri = [2]string {"usare un tunnel", "usare un area PI"}
-	var risorsa int
-	for i := 0; i < 2; i++ {
-		if tipo == 2 || tipo == i {
-			fmt.Printf("[CLIENTE %03d] mi metto in coda per %s\n", id, desideri[i])
-			entraCliente[i] <- r
-			risorsa = <-r.ack
-			fmt.Printf("[CLIENTE %03d] è il mio turno di %s numero %03d\n", id, desideri[i], risorsa)
-			time.Sleep(time.Duration(rand.Intn(TEMPO_CLIENTE)+TEMPO_MIN) * time.Millisecond)
-			fmt.Printf("[CLIENTE %03d] mi metto in coda per %s \n", id, desideri[i])
-			esceCliente[i] <- r
-			risorsa = <-r.ack
-			fmt.Printf("[CLIENTE %03d] è il mio turno di %s numero %03d\n", id, desideri[i], risorsa)
+	var (
+		r = richiesta { soggetto: id, oggetto: NON_SIGNIFICATIVO, ack: make(chan int) }
+		azioni = [TIPI_CLIENTE][AZIONI_CLIENTE]string {
+				{"usare un tunnel", "liberare il tunnel"},
+				{"usare un area PI", "liberare l'area PI"}}
+		risorsa int
+	)
+	for t := 0; t < TIPI_CLIENTE; t++ {
+		risorsa = NON_SIGNIFICATIVO
+		if tipo == 2 || tipo == t {
+			for i := 0; i < AZIONI_CLIENTE; i++ {
+				time.Sleep(time.Duration(rand.Intn(TEMPO_CLIENTE) + TEMPO_MINIMO) * time.Millisecond)
+				r.oggetto = risorsa
+				fmt.Printf("[%s %03d] mi metto in coda per %s\n", nome, id, azione[t][i])
+				canaliCliente[t][i] <- r
+				risorsa = <-r.ack
+				if risorsa < 0 {
+					finito <- true
+					fmt.Printf("[%s %03d] fine\n", nome, id)
+					return
+				}
+				fmt.Printf("[%s %03d] è il mio turno di %s\n numero %d", nome, id, azione[t][i], risorsa)
+			}
 		}
 	}
+	
 	finito <- true
-	fmt.Printf("[CLIENTE %03d] fine\n", id)
+	fmt.Printf("[%s %03d] fine\n", nome, id)
 }
 
 func main() {
 	fmt.Println("[MAIN] inizio")
 	rand.Seed(time.Now().Unix())
 	
-	// inizio goroutines
 	go autolavaggio()
-	for i := 0; i < NUM_ADDETTI; i++ {
-		go addetto(i)
-	}
-	for i := 0; i < NUM_CLIENTI; i++ {
-		go cliente(i, i % 3)
-	}
+	for i := 0; i < NUM_ADDETTI; i++ { go addetto(i) }
+	for i := 0; i < NUM_CLIENTI; i++ { go cliente(i, i % 3) }
 	
-	// fine goroutines
-	for i := 0; i < NUM_CLIENTI; i++ {
-		<-finito
-	}
+	for i := 0; i < NUM_CLIENTI; i++ { <-finito }
 	bloccaAutolavaggio <- true
-	for i := 0; i < NUM_ADDETTI; i++ {
-		<-finito
-	}
+	for i := 0; i < NUM_ADDETTI; i++ { <-finito }
 	terminaAutolavaggio <- true
 	<-finito
 	
